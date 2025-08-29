903ebf0e0e06f5f1924727e5d8f071b00a99c5a7
zakhar <zakhar.savchyn.dev@gmail.com>
2025-08-29 11:37:12 +0300

    added main.tf with resource_group_storage module

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.41.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+module "resource_group_storage" {
+  source  = "app.terraform.io/zave52/resource_group_storage/azurerm"
+  version = "1.0.1"
+
+  resource_group_name  = "my-rg"
+  location             = "East US"
+  storage_account_name = "mystorageaccount123"
+}
